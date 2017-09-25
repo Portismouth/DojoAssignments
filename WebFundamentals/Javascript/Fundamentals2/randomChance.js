13719b2dad8896f6a6cd85c1d4adadd6f660005d
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-09-25 14:32:57 -0500

    Slot machine game

--- WebFundamentals/Javascript/Fundamentals2/randomChance.js
+++ WebFundamentals/Javascript/Fundamentals2/randomChance.js
@@ -2,19 +2,22 @@ function slots(quarters, pass){
     while(quarters > 0){
         quarters -- 
         var win = 10;
-        var rand = Math.trunc(Math.random() * 100);
+        var rand = Math.trunc(Math.random() * 100); // Creates random number so see if winner
        if ( quarters > pass) {
+            // Checks if you have won enough quarts to quit
             return quarters;
             break;
         } else if ( win == rand) {
+            // If you won add the random number from 50 - 100 of quarters you won to your current amount
             var amount = Math.trunc(Math.random() * 50) + 50;
             quarters = quarters + amount;
         } else if ( quarters == 0 ) {
+            // All out of Quarters :(
             return 0;
         }
     }
 }
-var quarters = 10;
-var pass = 200;
+var quarters = 10; // Amount of Starting Quarters
+var pass = 200; // If you Make this much, Stop playing
 
 console.log(slots(quarters, pass))
