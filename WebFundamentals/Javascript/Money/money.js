5fee84e64fbadeb6703fd3ca0028428d1526eb78
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-09-25 12:13:54 -0500

    Added check for how many days

--- WebFundamentals/Javascript/Money/money.js
+++ WebFundamentals/Javascript/Money/money.js
@@ -1,17 +1,27 @@
-function thirty(days){
+function checkMoney(days){
     var money = 0;
     for(var i = 1; i <= days; i++){
         if(i == 1){
             money = 0.01;
-            console.log(i);
-            console.log(money);
         } else {
             money = money * 2;
-            console.log(i);
-            console.log(money);
         }
     }
     console.log(money)
 }
+
+
+function checkDays(total){
+    var checkdays = 1;
+    var checkMoney = 0.01;
+    while(checkMoney <= total){
+        checkMoney = checkMoney + checkMoney;
+        checkdays ++;
+    }
+    console.log(checkdays)
+}
+
 var days = 30;
-thirty(days);
+var total= Infinity;
+checkMoney(days);
+checkDays(total);
