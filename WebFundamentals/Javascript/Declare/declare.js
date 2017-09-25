7753a3c1c1b6934c12b75428f7537a61b8e11013
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-09-25 08:43:47 -0500

    Javascript Declaring Variables

---
+++ WebFundamentals/Javascript/Declare/declare.js
@@ -0,0 +1,18 @@
+var age = 23;
+var birthMonth = 2;
+var birthDay = 11;
+var birthYear = 1994;
+var firstName = 'Mitchell';
+var lastName = 'Morison';
+var greeting = 'Hello!';
+var adult = true;
+var child = false;
+
+var undefined;
+
+console.log('My age is ' + age);
+console.log('I was born ' + birthMonth + '/' + birthDay + '/' + birthYear);
+console.log(greeting + ' My name is ' + firstName + '' + lastName);
+console.log(adult);
+console.log(child);
+console.log(undefined);
