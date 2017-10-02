b60db1eb2a11aa2978e134f6d8e12b9d87ca942c
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-02 15:55:38 -0500

    Multiplication table

---
+++ Python_work/multiplication/multiply.py
@@ -0,0 +1,9 @@
+def table():
+    print "x 1 2 3 4 5 6 7 8 9 10 11 12"
+    for i in range(1,13):
+        print i,
+        for j in range(1,13):
+            num = i * j 
+            print num,
+        print "\n"  
+table()
