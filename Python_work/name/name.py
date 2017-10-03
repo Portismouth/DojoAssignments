89d1ef6bc949c3fd27a9a16fc4a5ce51993998fe
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-03 13:07:46 -0500

    iterating over nested ditionaries python

---
+++ Python_work/name/name.py
@@ -0,0 +1,36 @@
+
+# Part 1
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+def names(lst):
+    for student in lst:
+        print student['first_name'], student['last_name']
+
+
+# names(students)
+
+
+# Part 2
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def names(lst):
+    for key, value in lst.iteritems():
+        print key
+        for names in value:
+            print value.index(names)+1, "-",names['first_name'], names['last_name'], "-", len(names['first_name'])+len(names['last_name'])
+names(users)
