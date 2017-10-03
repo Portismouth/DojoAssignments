2f6ec9ccf80ffd7cc48729fc634547e96bfa2137
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-03 13:30:09 -0500

    converting key/values to tuple pairs

---
+++ Python_work/making_tuples/tuple.py
@@ -0,0 +1,11 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+def dict_tuple(dictionary):
+    tuple_list = []
+    for key, value in dictionary.iteritems():
+        tuple_list.append((key,value))
+    print tuple_list
+dict_tuple(my_dict)
