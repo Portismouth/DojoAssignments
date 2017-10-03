4729414f613ccab88348d4d0bb146f9d96d3b180
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-03 12:48:26 -0500

    iterating over a dictionary

---
+++ Python_work/dictionaries/dictionary.py
@@ -0,0 +1,15 @@
+
+
+mitchell = {
+    "name" : "Mitchell",
+    "age" : "23",
+    "country" : "USA",
+    "language" : "Pthon"
+}
+
+
+def dictionary(dct):
+    for key, value in dct.items():
+        print "My",key,"is",value
+
+dictionary(mitchell)
