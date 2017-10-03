d8d43c1f6334b12360d2df99c5d180356c601efd
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-03 12:24:13 -0500

    stars

---
+++ Python_work/stars/stars.py
@@ -0,0 +1,14 @@
+
+def draw_stars(x):
+    
+    for item in x:
+        temp = ""        
+        if isinstance(item, int):
+            for i in range(item):
+                temp += "*"
+        elif isinstance(item, str):
+            for i in range(len(item)):
+                temp += item[0]
+        print temp
+x = [10 , "hello"]
+draw_stars(x) 
