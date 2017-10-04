9dfb273c31cc523af8df765ebe1c18779390f24f
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-04 16:26:03 -0500

    Call center and waitlist

--- Python_work/python_OOP/call_center.py
+++ Python_work/python_OOP/call_center.py
@@ -1,10 +1,12 @@
 class Call(object):
-    def __init__(self, id_number, name, number, time, reason):
-        self.id_number = id_number
+    counter = 0
+    def __init__(self, name, number, time, reason):
         self.name = name
         self.number = number
         self.time = time
         self.reason = reason
+        self.id_number = Call.counter
+        Call.counter += 1
     def display(self):
         print "ID: {}\nName: {}\nNumber: {}\nTime: {}\nReason: {}\n".format(self.id_number, self.name, self.number, self.time, self.reason)
         return self
@@ -12,7 +14,6 @@ class Call(object):
 class CallCenter(object):
     def __init__(self):
         self.calls = []
-        self.wait_list = len(self.calls)
     def add(self, call):
         self.calls.append(call)
         return self
@@ -30,13 +31,14 @@ class CallCenter(object):
     def info(self):
         for info in self.calls:
             info.display()
+        print len(self.calls)
         return self
 
-call1 = Call(1,"Mitchell","435-680-8818","151","help")
-call2 = Call(2,"Mitc","435-680-8828","251","hi")
-call3 = Call(3,"Mit","435-680-8838","351","hi")
-call4 = Call(4,"Mitch","435-680-8848","451","help")
-call5 = Call(5,"Mitchel","435-680-8858","551","hi")
+call1 = Call("Mitchell","435-680-8818","1:51","help")
+call2 = Call("Myles","435-680-8828","2:51","hi")
+call3 = Call("John","435-680-8838","3:51","hi")
+call4 = Call("Jane","435-680-8848","4:51","help")
+call5 = Call("Diana","435-680-8858","5:51","hi")
 
 center = CallCenter()
 center.add(call1).add(call3).remove().add(call2).remove_number("435-680-8828").sort().info()
