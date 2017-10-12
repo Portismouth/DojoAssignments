1a29fe8fb04f10c26d5ebeb6e228e704a8c395c9
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-12 09:13:42 -0500

    SALTY LOGIN HASH

--- Python_work/flask_MySQL/login_and_registration/server.py
+++ Python_work/flask_MySQL/login_and_registration/server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 import md5
 import re
+import os, binascii
+
 app = Flask(__name__)
 mysql = MySQLConnector(app,'login')
 app.secret_key = 'KeepItSecretKeepItSafe'
@@ -41,9 +43,11 @@ def register():
         return redirect('/')
     if success:
         password = md5.new(password).hexdigest()
-        insert_query = "INSERT INTO users (first_name, last_name, email, password) VALUES ('{}','{}','{}','{}')".format(first_name, last_name, email, password)
+        salt =  binascii.b2a_hex(os.urandom(15))
+        hashed_pw = md5.new(password + salt).hexdigest()
+        insert_query = "INSERT INTO users (first_name, last_name, email, password, salt) VALUES ('{}','{}','{}','{}', '{}')".format(first_name, last_name, email, hashed_pw, salt)
         mysql.query_db(insert_query)
-        user_query = "SELECT * FROM users where users.email = '{}' AND users.password = '{}'".format(email,password)
+        user_query = "SELECT * FROM users where users.email = '{}' AND users.password = '{}'".format(email,hashed_pw)
         session['user'] = mysql.query_db(user_query)[0]
         return redirect('/success')
     
@@ -54,15 +58,20 @@ def login():
     session['user'] = ''
     password = md5.new(request.form['password']).hexdigest()
     email = request.form['email']
-    user_query = "SELECT * FROM users where users.email = '{}' AND users.password = '{}'".format(email,password)
-    user = mysql.query_db(user_query)[0]
-    print user
-    if not user:
+    user_query = "SELECT * FROM users WHERE users.email = :email LIMIT 1"
+    query_data = {'email': email}
+    user = mysql.query_db(user_query, query_data)
+    if len(user) != 0:
+        encrypted_password = md5.new(password + user[0]['salt']).hexdigest()
+        if user[0]['password'] == encrypted_password:
+            session['user'] = user[0]
+            return redirect('/success')
+        else:
+            flash('Invalid Password', 'login')
+            return redirect('/')
+    else:
         flash('Login not successful', 'login')
         return redirect('/')
-    else:
-        session['user'] = user
-        return redirect('/success')
 
 @app.route('/logout', methods=['POST'])
 def logout():
