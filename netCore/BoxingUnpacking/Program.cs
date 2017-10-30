9c799e3892371d3bb1de357c91b8e87e0278ee9f
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-30 15:02:38 -0500

    Unboxing

---
+++ netCore/BoxingUnpacking/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnpacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> box = new List<object>();
+            box.Add(78);
+            box.Add(-1);
+            box.Add(28);
+            box.Add(true);
+            box.Add("chair");
+            int sum = 0;
+            foreach ( var item in box )
+            {
+                if ( item is int )
+                {
+                    int j = (int)item;
+                    sum += j;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
