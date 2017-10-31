1389a38856da49d230999f2c941bf8a3e97cf8f1
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-31 12:04:58 -0500

    simple human object game

---
+++ netCore/human/Human.cs
@@ -0,0 +1,47 @@
+namespace human
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Dexterity { get; set; }
+        public int Health { get; set; }
+
+        public Human(string name, int strength = 3, int intelligence = 3, int dexterity = 3, int health = 100)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = health;
+        }
+
+        public void setAttributes(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = health;
+        }
+
+        public string attack(object npc, int damage)
+        {
+            if (npc is Human)
+            {
+                Human player = (Human)npc;
+                damage = damage * 5;
+                player.Health -= damage;
+                return $"{player.Name} took {damage} damage!";
+
+            }
+            else
+            {
+                return "You cannot attack attack non-human players";
+            }
+                    
+        }
+
+    }
+}
---
+++ netCore/human/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human mitchell = new Human("Mitchell");
+            Human spiderman = new Human("Spiderman");
+            mitchell.setAttributes("Mitchell", 10, 25, 7, 500);
+            string result = mitchell.attack(spiderman, mitchell.Strength);
+            Console.WriteLine(result);
+            Console.WriteLine("{0}'s health is now: {1}", spiderman.Name, spiderman.Health);
+        }
+    }
+}
